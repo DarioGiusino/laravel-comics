67c49cdd7ba836f60026a4795fbc550e1aba0272
DarioGiusino <dario.giusino@gmail.com>
2023-02-24 16:34:02 +0100

    added main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,7 +44,25 @@
   <section id="jumbo"></section>
 
   {{-- # main --}}
-  <main></main>
+  <main>
+    <div class="container">
+      <h3>current series</h3>
+      <div class="card-container">
+        @foreach (config('comics') as $comic)
+          <div class="comic-card">
+            <figure>
+              <img src="{{ $comic['thumb'] }}" alt="{{ $comic['type'] }}">
+            </figure>
+            <div class="card-text">
+              <h4>{{ $comic['series'] }}</h4>
+              <sub>{{ $comic['price'] }}</sub>
+            </div>
+          </div>
+        @endforeach
+      </div>
+      <button>load more</button>
+    </div>
+  </main>
 
   {{-- #footer --}}
   <footer>
