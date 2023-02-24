43e97ce9b651ea392a0aa49e6e8dc32a6e4515d4
DarioGiusino <dario.giusino@gmail.com>
2023-02-24 09:11:15 +0100

    styled complete header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Comics | Home</title>
+    {{-- fontawesome --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css' integrity='sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==' crossorigin='anonymous'/>
 
     @vite('resources/js/app.js')
 </head>
 <body>
-    <h1>ciao</h1>
+    <header class="container">
+        <figure>
+            <a href="{{url('/')}}">
+                <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="dc logo">
+            </a>
+        </figure>
+
+        <nav>
+            <ul>
+                @foreach (config('headermenu') as $link)
+                <li><a href="#" class="{{ $link['current'] ? 'current' : '' }}">{{ $link['text'] }}</a></li>
+                @endforeach
+            </ul>
+        </nav>
+        <form class="d-flex" role="search">
+            <input type="search" placeholder="Search">
+            <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+    </header>
     
 </body>
 </html>
